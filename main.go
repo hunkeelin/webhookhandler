1456e3a17a0af2cd2d47ced229b96664183f6289
klin Puppet-managed User <klin@ec2-klin-test-1.squaretrade.com>
2018-05-09 22:59:59 -0700

    bla

--- main.go
+++ main.go
@@ -5,18 +5,22 @@ import (
     "log"
     "encoding/json"
 )
-func handleWebHook(w http.ResponseWriter, r *http.Request) {
-    config,_ := readconfig()
+type Conn struct {
+    regex string
+}
+func (f *Conn) handleWebHook(w http.ResponseWriter, r *http.Request) {
     var gitpayload Gitpayload
     json.NewDecoder(r.Body).Decode(&gitpayload)
-    if matchstring(gitpayload.Compare,config[2]) {
+    if matchstring(gitpayload.Compare,f.regex) {
         fmt.Println("match!")
     }
     fmt.Println(r.Header["X-Github-Event"][0])
 }
 func main() {
+    newcon := new(Conn)
     config,_ := readconfig()
-    http.HandleFunc("/", handleWebHook)
+    newcon.regex = config[2]
+    http.HandleFunc("/", newcon.handleWebHook)
     err := http.ListenAndServeTLS(config[4]+":"+config[5],config[0],config[1], nil)
     if err != nil {
         log.Fatal("Unable to Listen to port", err)
--- readconfig.go
+++ readconfig.go
@@ -8,51 +8,52 @@ import (
 
 func readconfig() (toreturn []string, concur int) {
     var to_return []string
+    confpath := "/etc/genkins/genkins.conf"
 
     config := klinenv.NewAppConfig("genkins.conf")
 
     certdir, err := config.Get("certpath")
     if err != nil {
-        log.Fatal("unable to retrieve the value of certdir check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of certdir check config file " + confpath)
     }
     to_return = append(to_return,certdir)
     keydir, err := config.Get("keypath")
     if err != nil {
-        log.Fatal("unable to retrieve the value of certdir check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of certdir check config file " + confpath)
     }
     to_return = append(to_return,keydir)
 
     remote, err := config.Get("giturl")
     if err != nil {
-        log.Fatal("unable to retrieve the value of remote check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of remote check config file " + confpath)
     }
     to_return = append(to_return,remote)
 
     lockfile, err := config.Get("lockfile")
     if err != nil {
-        log.Fatal("unable to retrieve the value of lockfile check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of lockfile check config file " + confpath)
     }
     to_return = append(to_return,lockfile)
 
     bindaddr, err := config.Get("bindaddr")
     if err != nil {
-        log.Fatal("unable to retrieve the value of bindaddr check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of bindaddr check config file " + confpath)
     }
     to_return = append(to_return,bindaddr)
 
     port, err := config.Get("port")
     if err != nil {
-        log.Fatal("unable to retrieve the value of bindaddr check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of bindaddr check config file " + confpath)
     }
     to_return = append(to_return,port)
 
     rconcur, err := config.Get("concur")
     if err != nil {
-        log.Fatal("unable to retrieve the value of concur check config file /etc/genkins/genkins.conf")
+        log.Fatal("unable to retrieve the value of concur check config file " + confpath)
     }
     to_return_int, err := strconv.Atoi(rconcur)
     if err != nil {
-        log.Fatal("can't convert string to int for concur in /etc/genkins/genkins.conf")
+        log.Fatal("can't convert string to int for concur in " + confpath)
     }
     return to_return, to_return_int
 }

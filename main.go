1189f8af2394cc86c6bfe22e5978a4681b4c55d7
bgops <bgops@test1.klin-pro.com>
2018-05-25 21:19:15 -0700

    asdF

--- main.go
+++ main.go
@@ -9,32 +9,33 @@ import (
 )
 
 func (f *Conn) handleWebHook(w http.ResponseWriter, r *http.Request) {
-    switch apikey := r.Header.Get("api-key"); apikey {
-        case "": //github json by defualt
-            msg,status := GitWork(r,f)
-            w.WriteHeader(status)
-            w.Write([]byte(msg))
-            fmt.Println(msg,status)
-            return
-        case f.apikey: // application specific.
-            fmt.Println("right apikey")
-            return
-        default: // application specific but wrong api key. 
-            fmt.Println("wrong apikey")
-            w.WriteHeader(300)
-            w.Write([]byte("Wrong apikey\n"))
-            return
-    }
-    //f.mu.Lock()
-    //dowork()
-    ////f.mu.Unlock()
+	fmt.Println(r.RemoteAddr)
+	switch apikey := r.Header.Get("api-key"); apikey {
+	case "": //github json by defualt
+		msg, status := GitWork(r, f)
+		w.WriteHeader(status)
+		w.Write([]byte(msg))
+		fmt.Println(msg, status)
+		return
+	case f.apikey: // application specific.
+		fmt.Println("right apikey")
+		return
+	default: // application specific but wrong api key.
+		fmt.Println("wrong apikey")
+		w.WriteHeader(300)
+		w.Write([]byte("Wrong apikey\n"))
+		return
+	}
+	//f.mu.Lock()
+	//dowork()
+	////f.mu.Unlock()
 }
 func main() {
 	newcon := new(Conn)
 	// define config params
 	c := readconfig()
-    sema := make(chan struct {},1)
-    newcon.sem = sema
+	sema := make(chan struct{}, 1)
+	newcon.sem = sema
 	newcon.apikey = c.apikey
 	newcon.concur = c.concur
 	newcon.jobdir = c.jobdir
@@ -58,8 +59,8 @@ func main() {
 		IdleTimeout:  120 * time.Second,
 	}
 	fmt.Println("listening to " + c.bindaddr + " " + c.port)
-	err := s.ListenAndServeTLS(c.certpath, c.keypath)
-	//err := s.ListenAndServe()
+	//err := s.ListenAndServeTLS(c.certpath, c.keypath)
+	err := s.ListenAndServe()
 	if err != nil {
 		log.Fatal("can't listen and serve check port and binding addr")
 	}
